3872cf7595c7977100ec53ed3dabc95988c6a133
Leon Rode <leon.rode13@gmail.com>
2021-06-23 00:27:09 -0400

    Create 339A - Helpful Maths.cpp

---
+++ 339A - Helpful Maths.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    
+    vector<char> n;
+    
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] != '+') n.push_back(s[i]);
+    }
+    
+    sort(n.begin(), n.end());
+    
+    for (int i = 0; i < n.size(); i++) {
+        cout << n[i];
+        if (i != n.size()-1) {
+            cout << "+";
+        }
+    }
+}
